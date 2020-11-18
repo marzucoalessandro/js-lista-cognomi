70a95fcf38ba972a761dc80016997dfcc34cdc81
Alessandro <marzucoalessandro1@gmail.com>
2020-11-18 14:57:05 +0100

    inizio

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//Lista Cognomi
+//Chiedere all’utente il cognome
+//inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+//stampa la lista ordinata alfabeticamente
+//scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+//Consigli del giorno:
+//1. consultiamo la documentazione W3Schools o MDN per trovare i metodi javascript che possono esserci utili.
